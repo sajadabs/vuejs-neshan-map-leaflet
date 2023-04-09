ad4bfafa4d62d39de66725715f57bbcc7e950cc8
Sajad Abasi <sab3053@gmail.com>
2023-04-09 19:44:27 +0430

    new structure

--- index.js
+++
@@ -1,9 +0,0 @@
-import NeshanMapLeaflet from "./src/NeshanMapLeaflet.vue";
-
-export default {
-    install(Vue, options) {
-        // Let's register our component globally
-        // https://vuejs.org/v2/guide/components-registration.html
-        Vue.component("NeshanMapLeaflet", NeshanMapLeaflet);
-    }
-};
